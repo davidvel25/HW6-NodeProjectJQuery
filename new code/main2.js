a57f4cd3ecbe367306f3fccb29177069a702ddf6
davidvel25 <davidvel25@outlook.com>
2022-05-19 15:16:22 -0700

    need to add descriptions to pages + comments

--- new code/main2.js
+++ new code/main2.js
@@ -1,56 +1,56 @@
-let libraryArray = [];
+// let libraryArray = [];
 
-let Book = function (bTitle, bYear, bLastName, bFirstName, bISBN, bGenre){
-    this.title = bTitle;
-    this.year = bYear;
-    this.lastName = bLastName;
-    this.firstName = bFirstName;
-    this.libraryCount = libraryArray.length + 1;
-    this.ISBN = bISBN;
-    this.genre = bGenre;
-}
+// let Book = function (bTitle, bYear, bLastName, bFirstName, bISBN, bGenre){
+//     this.title = bTitle;
+//     this.year = bYear;
+//     this.lastName = bLastName;
+//     this.firstName = bFirstName;
+//     this.libraryCount = libraryArray.length + 1;
+//     this.ISBN = bISBN;
+//     this.genre = bGenre;
+// }
 
-movieArray.push(new MovieObject("book1", 2012, "floop", "droop", 1925320, comedy));
+// movieArray.push(new MovieObject("book1", 2012, "floop", "droop", 1925320, comedy));
 
-let currentGenre = "noneSelected";
+// let currentGenre = "noneSelected";
 
-document.addEventListener("DOMContentLoaded", function(){
-    createList();
+// document.addEventListener("DOMContentLoaded", function(){
+//     createList();
 
-    document.getElementById("addBook").addEventListener("click", function(){
-        libraryArray.push(new Book  (   document.getElementById("title").value,
-                                        document.getElementById("year").value,
-                                        document.getElementById("lastName").value,
-                                        document.getElementById("firstName").value,
-                                        document.getElementById("ISBN").value,
-                                        document.getElementById("genre").value
-                                    ));
-    document.location.href = "index.html#listAll";
-    });
+//     document.getElementById("addBook").addEventListener("click", function(){
+//         libraryArray.push(new Book  (   document.getElementById("title").value,
+//                                         document.getElementById("year").value,
+//                                         document.getElementById("lastName").value,
+//                                         document.getElementById("firstName").value,
+//                                         document.getElementById("ISBN").value,
+//                                         document.getElementById("genre").value
+//                                     ));
+//     document.location.href = "index.html#listAll";
+//     });
 
-    document.getElementById("buttonClear").addEventListener("click", function(){
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
-        document.getElementById("lastName").value = "";
-        document.getElementById("firstName").value = "";
-        document.getElementById("ISBN").value = "";
-        // document.getElementById("genre").value = "";    
-    });
+//     document.getElementById("buttonClear").addEventListener("click", function(){
+//         document.getElementById("title").value = "";
+//         document.getElementById("year").value = "";
+//         document.getElementById("lastName").value = "";
+//         document.getElementById("firstName").value = "";
+//         document.getElementById("ISBN").value = "";
+//         // document.getElementById("genre").value = "";    
+//     });
 
-    $(document).bind("change", "select-genre", function(event, ui){
-        selectedGenre = $('select-genre').val();
-    });
+//     $(document).bind("change", "select-genre", function(event, ui){
+//         selectedGenre = $('select-genre').val();
+//     });
 
-    document.getElementById("buttonSortTitle").addEventListener("click", function(){
-        movieArray.sort(dynamicSort("title"));
-        createList();
-        document.location.href = "index.html#listAll";
-    });
+//     document.getElementById("buttonSortTitle").addEventListener("click", function(){
+//         movieArray.sort(dynamicSort("title"));
+//         createList();
+//         document.location.href = "index.html#listAll";
+//     });
 
-    document.getElementById("buttonSortGenre").addEventListener("click", function(){
-        movieArray.sort(dynamicSort("genre"));
-        createList();
-        document.location.href = "index.html#listAll";
-    });
+//     document.getElementById("buttonSortGenre").addEventListener("click", function(){
+//         movieArray.sort(dynamicSort("genre"));
+//         createList();
+//         document.location.href = "index.html#listAll";
+//     });
 
-});
+// });
