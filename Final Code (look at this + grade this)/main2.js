cb3c143d7c35434611d2107e429213339187b463
davidvel25 <davidvel25@outlook.com>
2022-05-24 19:33:04 -0700

    finished details page (no comments)

--- Final Code (look at this + grade this)/main2.js
+++ Final Code (look at this + grade this)/main2.js
@@ -11,6 +11,7 @@ let Book = function (bTitle, bYear, bLastName, bFirstName, bISBN, bGenre, bRevie
     this.reviewer = bReviewer;
     this.date = bDate;
     this.rating = bRating;
+    this.ID = Math.random().toString(16).slice(5);
 
     //Rating Catalog
     this.newReview = [new Review(bDate, bTitle, bGenre, bReviewer, bRating)];
@@ -24,7 +25,7 @@ let Review = function(bDate, bTitle, bGenre, bReviewer, bRating){
     this.rating = bRating;
 }
 
-libraryArray.push(new Book("book1", 2012, "floop", "droop", 1925320, 'adventure', 'John Dempsey', '04/13/2015', '5'));
+libraryArray.push(new Book("book1", 2012, "floop", "droop", 1925320, 'adventure', 'John Dempsey', '2022-05-04', '5'));
 
 let currentGenre = "noneSelected";
 
@@ -76,21 +77,113 @@ document.addEventListener("DOMContentLoaded", function(){
         let fullList = "";
         for (let element of libraryArray) {
             fullList += `   
-                            <li><strong><u>Book #${element.libraryCount}</u></strong>:</li>
-                            <li>title: ${element.title}</li>
-                            <li>year: ${element.year}</li>
-                            <li>last name: ${element.lastName}</li>
-                            <li>first name: ${element.firstName}</li>
-                            <li>isbn: ${element.ISBN}</li>
-                            <li>genre: ${element.genre}</li>
-                            <li>reviewer: ${element.reviewer}</li>
-                            <li>date: ${element.date}</li>
-                            <li>rating: ${element.rating}</li>
+                            <li>${element.date} : ${element.title} by ${element.lastName},${element.firstName} rated by ${element.reviewer} with a rating of ${element.rating}</li>
                             <br>
                         `;
+
+            // fullList += `   
+            //                 <li><strong><u>Book #${element.libraryCount}</u></strong>:</li>
+            //                 <li>title: ${element.title}</li>
+            //                 <li>year: ${element.year}</li>
+            //                 <li>last name: ${element.lastName}</li>
+            //                 <li>first name: ${element.firstName}</li>
+            //                 <li>isbn: ${element.ISBN}</li>
+            //                 <li>genre: ${element.genre}</li>
+            //                 <li>reviewer: ${element.reviewer}</li>
+            //                 <li>date: ${element.date}</li>
+            //                 <li>rating: ${element.rating}</li>
+            //                 <br>
+            //             `;
         }
         tempulList.innerHTML = fullList;
     }
+
+        // page before show code *************************************************************************
+        $(document).on("pagebeforeshow", "#listAll", function (event) {   // have to use jQuery 
+            createList();
+        });
+    
+    
+        // need one for our details page to fill in the info based on the passed in ID
+        $(document).on("pagebeforeshow", "#details", function (event) {   
+            let BookID = localStorage.getItem('parm');  // get the unique key back from the storage dictionairy
+            let book;
+            for (let i = 0; i < libraryArray.length; i++){
+                if(libraryArray[i].ID == BookID){
+                    book = libraryArray[i];
+                    document.getElementById("specificBookID").innerHTML = BookID;
+                    document.getElementById("specificBookRating").innerHTML = book.rating;
+                    document.getElementById("specificBookTitle").innerHTML = book.title;
+                }
+            }
+            // document.getElementById("specificBookID").innerHTML = "Title: " + title;
+            // document.getElementById("specificBookID").innerHTML = "Year Published: " + year;
+            // document.getElementById("specificBookID").innerHTML = "Author's Last Name: " + lastName;
+            // document.getElementById("specificBookID").innerHTML = "Author's First Name: " + firstName;
+            // document.getElementById("specificBookID").innerHTML = "ISBN: " + ISBN;
+            // document.getElementById("specificBookID").innerHTML = "Genre: " + genre;
+            // document.getElementById("specificBookID").innerHTML = "Reviewer: " + reviewer;
+            // document.getElementById("specificBookID").innerHTML = "Date: " + date;
+            // document.getElementById("specificBookID").innerHTML = "Rating: " + rating;
+
+            // document.getElementById("title").innerHTML = "Title: " + title;
+            // document.getElementById("year").innerHTML = "Year Published: " + year;
+            // document.getElementById("lastName").innerHTML = "Author's Last Name: " + lastName;
+            // document.getElementById("firstName").innerHTML = "Author's First Name: " + firstName;
+            // document.getElementById("ISBN").innerHTML = "ISBN: " + ISBN;
+            // document.getElementById("select-genre").innerHTML = "Genre: " + genre;
+            // document.getElementById("reviewer").innerHTML = "Reviewer: " + reviewer;
+            // document.getElementById("date").innerHTML = "Date: " + date;
+            // document.getElementById("rating").innerHTML = "Rating: " + rating;
+        });
+
+    function createList() {
+        // clear prior data
+        var theList = document.getElementById("ulBookList");
+        theList.innerHTML = "";
+    
+     
+    
+        libraryArray.forEach(function (element, i) {   // use handy array forEach method
+            var myLi = document.createElement('li');
+            myLi.classList.add('oneBook');
+            myLi.innerHTML =  element.date + ":  " + element.title + " rated by " + element.reviewer + " with rating of " + element.rating;
+    
+            // use the html5 "data-parm" to store the ID of this particular movie object 
+            // that we are currently building an li for so that I can later know which movie this li came from
+            myLi.setAttribute("data-parm", element.ID);
+    
+            theList.appendChild(myLi);
+        });
+    
+        var liList = document.getElementsByClassName("oneBook");
+        let newLibraryArray = Array.from(liList);
+    
+        newLibraryArray.forEach(function (element,i) {     
+            element.addEventListener('click', function () {     
+                
+                var parm = this.getAttribute("data-parm");  // data-parm has this movie object's ID value
+                // now save THIS ID value in the localStorage "dictionairy"
+                localStorage.setItem('parm', parm);
+                document.location.href = "index2.html#details";
+            });
+        });
+      
+    };
+
+
+
+    // $(document).on("pagebeforeshow", "#details", function(event){
+        // document.getElementById("title").innerHTML = "Title: " + title;
+        // document.getElementById("year").innerHTML = "Year Published: " + year;
+        // document.getElementById("lastName").innerHTML = "Author's Last Name: " + lastName;
+        // document.getElementById("firstName").innerHTML = "Author's First Name: " + firstName;
+        // document.getElementById("ISBN").innerHTML = "ISBN: " + ISBN;
+        // document.getElementById("select-genre").innerHTML = "Genre: " + genre;
+        // document.getElementById("reviewer").innerHTML = "Reviewer: " + reviewer;
+        // document.getElementById("date").innerHTML = "Date: " + date;
+        // document.getElementById("rating").innerHTML = "Rating: " + rating;
+    //     })
 
     document.getElementById("sortTitle").addEventListener("click", function(){
         console.log("sort Title");
